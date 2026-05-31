d156f5d9c35463bc712c2b00290c465d4eae20ac
Zakiyu-99 <2425401214@live.gctu.edu.gh>
2026-05-31 21:42:30 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
+# File: initals.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 2
+#
+# Name: Alhassan Ahmed Zakiyu
+# STUDENT ID: 2425401214
+# Email: 2425401214@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+#
+# On my honor, Alhassan Ahmed Zakiyu, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials in large stylized letters.
 
+ print()
+ print("...AAZ")
+ print()
+
+ print("...AAAAAAAAAAAA........AAAAAAAAAAAA...........ZZZZZZZZZZZZZ.........")
+ print("...AAAA....AAAA........AAAA....AAAA....................ZZZ..........")
+ print("...AAAA....AAAA........AAAA....AAAA...................ZZZ...........")
+ print("...AAAA....AAAA........AAAA....AAAA..................ZZZ............")
+ print("...AAAA....AAAA........AAAA....AAAA................ZZZ..............")
+ print("...AAAAAAAAAAAA........AAAAAAAAAAAA..............ZZZ................")
+ print("...AAAAAAAAAAAA........AAAAAAAAAAAA............ZZZZ.................")
+ print("...AAAA....AAAA........AAAA....AAAA..........ZZZZ...................")
+ print("...AAAA....AAAA...**...AAAA....AAAA...**...ZZZZZ..................**")
+ print("...AAAA....AAAA...**...AAAA....AAAA...**...ZZZZZZZZZZZZZ..........**")
+
+
+ print()
 
 
 main()
